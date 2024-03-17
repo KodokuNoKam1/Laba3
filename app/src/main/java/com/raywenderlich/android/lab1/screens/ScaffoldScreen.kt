4e9ef6eaf0911d6014d0fce9c213213bccbbda53
KodokuNoKam1 <153433903+KodokuNoKam1@users.noreply.github.com>
2024-03-17 13:46:59 +0300

    add_MyTopAppBar_to_ScaffoldScreen

--- app/src/main/java/com/raywenderlich/android/lab1/screens/ScaffoldScreen.kt
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/ScaffoldScreen.kt
@@ -1,6 +1,5 @@
 package com.raywenderlich.android.lab1.screens
 
-import androidx.compose.material.ScaffoldState
 import androidx.compose.runtime.Composable
 import com.raywenderlich.android.lab1.router.BackButtonHandler
 import com.raywenderlich.android.lab1.router.FundamentalsRouter
@@ -10,8 +9,13 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.res.colorResource
 import com.raywenderlich.android.lab1.R
 import android.annotation.SuppressLint
-import androidx.compose.material.rememberScaffoldState
-import androidx.compose.material.Scaffold
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import kotlinx.coroutines.launch
+
 @Composable
 fun ScaffoldScreen(){
     MyScaffold()
@@ -43,7 +47,26 @@ fun MyScaffold(){
 
 @Composable
 fun MyTopAppBar(scaffoldState: ScaffoldState, scope: CoroutineScope){
+    val drawerState = scaffoldState.drawerState
 
+    TopAppBar(
+        navigationIcon = {
+            IconButton(
+                content = {
+                    Icon(
+                        Icons.Default.Menu,
+                        tint = Color.White,
+                        contentDescription = stringResource(id = R.string.menu)
+                    )
+                },
+                onClick = {
+                    scope.launch { if (drawerState.isClosed) drawerState.open() else drawerState.close() }
+                }
+            )
+        },
+        title = { Text(text = stringResource(id = R.string.MyComposeApp), color = Color.White )},
+        backgroundColor =  colorResource(id = R.color.colorPrimary)
+    )
 }
 
 @Composable
