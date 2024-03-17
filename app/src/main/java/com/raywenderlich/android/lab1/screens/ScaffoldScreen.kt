e249d508ef48b92d80588d3ec37f8d7a44a72337
KodokuNoKam1 <153433903+KodokuNoKam1@users.noreply.github.com>
2024-03-17 13:12:45 +0300

    ScaffoldScreen_add

--- app/src/main/java/com/raywenderlich/android/lab1/screens/ScaffoldScreen.kt
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/ScaffoldScreen.kt
@@ -1,11 +1,17 @@
 package com.raywenderlich.android.lab1.screens
 
-import androidx.compose.material.BackdropScaffoldState
 import androidx.compose.material.ScaffoldState
 import androidx.compose.runtime.Composable
 import com.raywenderlich.android.lab1.router.BackButtonHandler
 import com.raywenderlich.android.lab1.router.FundamentalsRouter
 import com.raywenderlich.android.lab1.router.Screen
+import kotlinx.coroutines.CoroutineScope
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.res.colorResource
+import com.raywenderlich.android.lab1.R
+import android.annotation.SuppressLint
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.material.Scaffold
 @Composable
 fun ScaffoldScreen(){
     MyScaffold()
@@ -16,12 +22,27 @@ fun ScaffoldScreen(){
 }
 
 @Composable
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 fun MyScaffold(){
+    val scaffoldState: ScaffoldState = rememberScaffoldState()
+    val scope: CoroutineScope = rememberCoroutineScope()
+
+
+    Scaffold(
+        scaffoldState = scaffoldState,
+        contentColor = colorResource(id = R.color.colorPrimary),
+        content = { MyRow() },
+        topBar = { MyTopAppBar(scaffoldState = scaffoldState, scope = scope) },
+        bottomBar = { MyBottomAppBar()},
+        drawerContent = { MyColumn()}
+
+
+    )
 
 }
 
 @Composable
-fun MyTopAppBar(scaffoldState: ScaffoldState){
+fun MyTopAppBar(scaffoldState: ScaffoldState, scope: CoroutineScope){
 
 }
 
