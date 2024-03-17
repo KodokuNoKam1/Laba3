12cc798543c224a81811e4f5611cf05cb5aea72d
KodokuNoKam1 <153433903+KodokuNoKam1@users.noreply.github.com>
2024-03-17 14:54:36 +0300

    GreedScreen_remake

--- app/src/main/java/com/raywenderlich/android/lab1/screens/GridScreen.kt
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/GridScreen.kt
@@ -1,6 +1,5 @@
 package com.raywenderlich.android.lab1.screens
 
-import androidx.compose.foundation.layout.RowScope
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.Composable
@@ -8,7 +7,6 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import com.raywenderlich.android.lab1.router.BackButtonHandler
 import com.raywenderlich.android.lab1.router.FundamentalsRouter
 import com.raywenderlich.android.lab1.router.Screen
-import kotlin.math.ceil
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -17,10 +15,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Icon
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 
 private val items = listOf(
     Icons.Filled.Check,
@@ -35,6 +33,7 @@ private val items = listOf(
     Icons.Filled.ThumbUp,
 )
 
+@ExperimentalFoundationApi
 @Composable
 fun GridScreen(){
     GridView(columnCount = 2)
@@ -46,53 +45,32 @@ fun GridScreen(){
 
 @Composable
 fun GridView(columnCount: Int) {
-    val itemSize = items.size
-    val rowCount = ceil(itemSize.toFloat() / columnCount).toInt()
-    val gridItems = mutableListOf<List<IconResource>>()
-    var position = 0
-    for (i in 0 until rowCount) {
-        val rowItem = mutableListOf<IconResource>()
-        for (j in 0 until columnCount) {
-            if (position.inc() <= itemSize) {
-                rowItem.add(IconResource(items[position++], true))
+    LazyVerticalGrid(
+        modifier = Modifier.fillMaxSize(),
+        columns = GridCells.Fixed(3) ,
+        content = {
+            items(count = items.size){item ->
+                GridIcon(IconResource(items[item], true))
             }
         }
-        val itemsToFill = columnCount - rowItem.size
-
-        for (j in 0 until itemsToFill) {
-            rowItem.add(IconResource(Icons.Filled.Delete, false))
-        }
-        gridItems.add(rowItem)
-    }
-    LazyColumn(modifier = Modifier.fillMaxSize()) {
-        items(gridItems) { items ->
-            RowItem(items)
-        }
-    }
-}
-
-@Composable
-    fun RowItem(rowItems: List<IconResource>) {
-        Row {
-            for (element in rowItems)
-                GridIcon(element)
-        }
-    }
-
-@Composable
-fun RowScope.GridIcon(iconResource: IconResource){
-    val color = if (iconResource.isVisible)
-        colorResource(R.color.colorPrimary)
-    else Color.Transparent
-
-    Icon(
-        imageVector = iconResource.imageVector,
-        tint = color,
-        contentDescription = stringResource(R.string.grid_icon),
-        modifier = Modifier
-            .size(80.dp, 80.dp)
-            .weight(1f)
     )
-}
+        BackButtonHandler {
+            FundamentalsRouter.navigateTo(Screen.Navigation)
+        }
+    }
+
+@Composable
+fun GridIcon(iconResource: IconResource){
+        val color = if (iconResource.isVisible)
+            colorResource(R.color.colorPrimary)
+        else Color.Transparent
+        Icon(
+            imageVector = iconResource.imageVector,
+            tint = color,
+            contentDescription = stringResource(R.string.grid_icon),
+            modifier = Modifier
+                .size(80.dp, 80.dp)
+        )
+    }
 
 data class IconResource(val imageVector: ImageVector, val isVisible: Boolean)
