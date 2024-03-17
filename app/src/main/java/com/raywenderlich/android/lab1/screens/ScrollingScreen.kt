daf4d603aa8cc5315a9e7e42742ae85feb1503af
KodokuNoKam1 <153433903+KodokuNoKam1@users.noreply.github.com>
2024-03-17 14:34:30 +0300

    image_horizontal_scroll_remake

--- app/src/main/java/com/raywenderlich/android/lab1/screens/ScrollingScreen.kt
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/ScrollingScreen.kt
@@ -14,10 +14,10 @@ import androidx.compose.ui.unit.dp
 import com.raywenderlich.android.lab1.router.BackButtonHandler
 import com.raywenderlich.android.lab1.router.FundamentalsRouter
 import com.raywenderlich.android.lab1.router.Screen
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import com.raywenderlich.android.lab1.R
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Row
 
 @Composable
 fun ScrollingScreen () {
@@ -30,7 +30,7 @@ fun ScrollingScreen () {
 
 @Composable
 fun MyScrollingScreen(modifier: Modifier = Modifier) {
-    Column(modifier = modifier.verticalScroll(rememberScrollState())) {
+    Row(modifier = modifier.horizontalScroll(rememberScrollState())) {
         BookImage(R.drawable.advanced_architecture_android, R.string.architecture)
         BookImage(R.drawable.kotlin_aprentice, R.string.learn_kotlin)
         BookImage(R.drawable.kotlin_coroutines, R.string.coroutines)
